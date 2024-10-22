6b74051f711177fa23423e1e3d2373345d617b1d
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-22 22:23:19 +0700

    refactor: add additional props: classname

--- src/components/ui/goBack.tsx
+++ src/components/ui/goBack.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import {Icon} from "@iconify/react";
 
-export default function GoBack({href}: {href: string}) {
+export default function GoBack({href, classname}: {href: string, classname?: string}) {
     return (
         <Link
             href={href}
-            className="absolute top-4 left-4 bg-white p-2 rounded-full shadow-lg text-black cursor-pointer hover:bg-gray-200 text-3xl"
+            className={`${classname} absolute top-4 left-4 bg-white p-2 rounded-full shadow-lg text-black cursor-pointer hover:bg-gray-200 text-3xl`}
         >
             <Icon icon="pajamas:go-back" />
         </Link>
