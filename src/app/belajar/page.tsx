07f6d84710f6698751d21374b51ee8cd2ba642e4
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-22 22:26:51 +0700

    feat: add slideshow to belajar page

--- src/app/belajar/page.tsx
+++ src/app/belajar/page.tsx
@@ -1,7 +1,43 @@
+import Slideshow from "@/components/slideShow";
+import GoBack from "@/components/ui/goBack";
+const slides = [
+    {
+        image: '/vehicle/car.png',
+        englishText: 'Car',
+        indonesiaText: 'Mobil',
+    },
+    {
+        image: '/vehicle/bus.png',
+        englishText: 'Bus',
+        indonesiaText: 'Bis',
+    },
+    {
+        image: '/vehicle/train.png',
+        englishText: 'Train',
+        indonesiaText: 'Kereta Api',
+    },
+    {
+        image: '/vehicle/angkot.png',
+        englishText: 'Public Transport',
+        indonesiaText: 'Angkutan Umum',
+    },
+    {
+        image: '/vehicle/ambulance.png',
+        englishText: 'Ambulance',
+        indonesiaText: 'Ambulan',
+    },
+    {
+        image: '/vehicle/truck.png',
+        englishText: 'Truck',
+        indonesiaText: 'Truk',
+    },
+];
+
 export default function Page() {
     return (
         <div className="">
-            <h1>Halaman Belajar</h1>
+            <GoBack href={'/menu'} classname="z-50" />
+            <Slideshow slides={slides} />
         </div>
     )
 }
