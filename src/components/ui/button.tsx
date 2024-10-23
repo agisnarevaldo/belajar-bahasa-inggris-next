355d39ac23b13749d854c898fa95252bf480f9d8
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-23 16:15:09 +0700

    refactor: change padding x

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,7 +11,7 @@ const Button = ({classname, href, children, txt1, txt}: {
         <div className={`${classname}`}>
             <Link
                 href={href}
-                className="flex items-center justify-start  gap-2 px-3 h-[40px] bg-[#A1FF14] text-[#131313] font-semibold rounded-[30px] border-none shadow-[5px_5px_10px_rgba(0,0,0,0.116)] transition-transform duration-200 cursor-pointer hover:bg-[#C0FF14] active:scale-95">
+                className="flex items-center justify-start gap-2 px-4 h-[40px] bg-[#A1FF14] text-[#131313] font-semibold rounded-[30px] border-none shadow-[5px_5px_10px_rgba(0,0,0,0.116)] transition-transform duration-200 cursor-pointer hover:bg-[#C0FF14] active:scale-95">
                 {txt1}
                 {children}
                 {txt}
