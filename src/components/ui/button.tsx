a284a81bf26304098217d6519b01ec1999130b3c
Agisna Revaldo <agisrifaldo75@gmail.com>
2025-01-20 20:52:23 +0700

    style: increase padding and font size for button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,7 +11,7 @@ const Button = ({classname, href, children, txt1, txt}: {
         <div className={`${classname}`}>
             <Link
                 href={href}
-                className="flex items-center justify-start gap-2 px-4 h-[40px] bg-[#A1FF14] text-[#131313] font-semibold rounded-[30px] border-none shadow-[5px_5px_10px_rgba(0,0,0,0.116)] transition-transform duration-200 cursor-pointer hover:bg-[#C0FF14] active:scale-95">
+                className="text-3xl font-bold flex items-center justify-start gap-4 px-6 py-6 h-[40px] bg-[#A1FF14] text-[#131313] rounded-[30px] border-none shadow-[5px_5px_10px_rgba(0,0,0,0.116)] transition-transform duration-200 cursor-pointer hover:bg-[#C0FF14] active:scale-95">
                 {txt1}
                 {children}
                 {txt}
